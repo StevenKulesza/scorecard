2b0c80dd6be63702ec44e150f344e529f0499d0f
Steven <info@stevenkulesza.com>
2017-09-23 17:34:39 -0400

    class to function application

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="scoreboard">
-        <div className="header">
-          <h1>Scoreboard</h1>
-        </div>
-
-        <div className="players">
-          <div className="player">
-            <div className="player-name">
-              Test User
-            </div>
-            <div className="player-score">
-              <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <div className="counter-score"> 21 </div>
-                <button className="counter-action increment"> + </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="players">
-          <div className="player">
-            <div className="player-name">
-              Test User 2
-            </div>
-            <div className="player-score">
-              <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <div className="counter-score"> 54 </div>
-                <button className="counter-action increment"> + </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>{props.title}</h1>
       </div>
-    );
-  }
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Test User
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 21 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Test User 2
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 54 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
-export default App;
+export default Application;
