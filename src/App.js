8ab163984aa801f122bbfa5c19ef139ee01147b4
Steven <info@stevenkulesza.com>
2017-09-23 18:04:09 -0400

    establish proptypes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function Application(props) {
-  return (
-    <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+Application.PropTypes = {
+  title: PropTypes.string
+};
 
-      <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Test User
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 21 </div>
-              <button className="counter-action increment"> + </button>
+Application.defaultProps = {
+  title: "Score Board"
+};
+
+class Application extends React.Component {
+  render() { return (
+      <div className="scoreboard">
+        <div className="header">
+          <h1>{this.props.title}</h1>
+        </div>
+
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+              Test User
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 21 </div>
+                <button className="counter-action increment"> + </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Test User 2
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 54 </div>
-              <button className="counter-action increment"> + </button>
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+              Test User 2
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 54 </div>
+                <button className="counter-action increment"> + </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Application;
