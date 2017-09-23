6e93426db87bf2c089f8c077724696bcffe7948b
Steven <info@stevenkulesza.com>
2017-09-23 17:20:01 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="application">
+        <div className="header">
+          <h1>Scoreboard</h1>
+        </div>
+
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+              Test User
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 21 </div>
+                <button className="counter-action increment"> + </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
